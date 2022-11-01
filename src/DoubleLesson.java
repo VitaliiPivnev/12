4b484677931810315b0244ab5730ed8ee6bd3ff4
Vitalii Pivnev <vitaliipivnev@MacBook-Pro-a.local>
2022-11-01 11:41:40 +0100

    Initial commit

---
+++ src/DoubleLesson.java
@@ -0,0 +1,32 @@
+public class DoubleLesson {
+    public static void main(String[] args) {
+        //double salary = 5.6;
+        //int workHoursPerDay = 12;
+        //int workDays = 364;
+        //int totalHours = workDays * workHoursPerDay;
+        //double totalEarned = totalHours * salary;
+        //System.out.println(totalHours);
+        //System.out.println(totalEarned); // зп без отпуска
+        //double vacation = 14; // количество дней отпуска
+        //double holidaySalary = salary / 2; //цена за час в отпуск
+        //double  vacationSalary = (vacation * workHoursPerDay) * holidaySalary; // сумма оплаты в отпуск
+        //System.out.println(vacationSalary);
+        //double workDay = workDays - vacation; //количество дней с учетом отпуска
+        //double Total = ((workDay * workHoursPerDay) * salary) + vacationSalary;
+        //System.out.println(Total); // ЗП с учетом отпуска
+
+
+        // символьный тип данных
+        char s; // 2 байта записывается только в одинарных ковычках, один символ например- 'a'
+
+        char symbol = 'a';
+        System.out.println(symbol);
+
+
+
+        // логический тип данных
+
+    }
+}
+
+
